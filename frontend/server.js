f0af380a1aee7d40e3439ff7786ab7d055bfd9dd
Viktor Herdinai <herdinai.viktor98@gmail.com>
2021-09-25 20:13:17 +0200

    fix post method

--- frontend/server.js
+++ frontend/server.js
@@ -3,12 +3,13 @@ const app = express();
 var path = require('path')
 const server = require('http').createServer(app);
 var cors = require('cors')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const PORT = 3000;
 
 
-const dbUrl = 'mongodb+srv://<user>:<password>@<cluster>.s0pds.mongodb.net/test';
+const dbUrl = 'mongodb+srv://admin:a_new_password_icanremember123@feedbackcluster.s0pds.mongodb.net/test';
 
 mongoose.connect(dbUrl);
 
@@ -20,7 +21,12 @@ mongoose.connection.on('error', (error) => {
     console.log('Hiba tortent', error);
 })
 
-var FeedbackModel = require('./feedback.model');
+require('./feedback.model');
+const FeedbackModel = mongoose.model('feedback');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({}));
+
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'static')));
@@ -29,9 +35,10 @@ app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/feedback', (req, res, next) => {
+app.post('/', (req, res, next) => {
+    console.log(req);
     if(req.body.user_id && req.body.description){
-        var feedback = new FeedbackModel(req.body.user_id, req.body.description);
+        const feedback = new FeedbackModel({user_id: req.body.user_id, description: req.body.description});
         feedback.save((err, doc) => {
             if (err) res.status(500).send("Adatbazis hiba");
             if (doc) res.status(200).send("Sikeres visszajelzes!");
